03dc5ff749eb890e88760b7e84b94b2e78b8aa4b
Ola Olsen <31655537+olaolsen22@users.noreply.github.com>
2018-07-06 11:48:59 +0800

    Replaced login screen with a splash screen

--- Modules/UserInterface/loginController.py
+++ Modules/UserInterface/loginController.py
@@ -1,36 +1,36 @@
-import random
+import random, time
 
-from PyQt5.QtCore import pyqtSlot, QByteArray
+from PyQt5.QtCore import pyqtSlot, QByteArray, QTimer, Qt
+from PyQt5.QtGui import QMovie
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 import iconpack
+from PyQt5.uic.properties import QtCore
 
 
 class LogInForm(QDialog):
     def __init__(self):
         super().__init__()
         loadUi('login_Form.ui', self)
-        self.setFixedSize(877, 625)
-        self.loginBtn.clicked.connect(self.openMainForm) #connects login btn to the method openWindow
+        self.setFixedSize(420, 550)
+        movie = QMovie("loading.gif")
+        self.logoIMG.setMovie(movie)
+        movie.start()
+        timer = QTimer()
+        timer.timeout.connect(self.openMainForm)
+        timer.start(6)
 
-    @pyqtSlot()
     def openMainForm(self): #for calling the main menu
         from mainController import MainForm
-
-        self.progressBar.setMinimumHeight(40)
-        self.completed = 0
-        while self.completed < 100:
-            self.completed += random.uniform(0.0001, 0.00006)
-            self.progressBar.setValue(self.completed)
-
         self.window = MainForm(self)
+        self.window.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
         self.hide()
         self.window.show()
-
 
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
     window = LogInForm()
+    window.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
     window.show()
     sys.exit(app.exec_())# program still runs even if you quit on login window
